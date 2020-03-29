607ad8502e3e1e64feda6d6037cc95f52f123b40
marcabeer <marcabeer@yahoo.com>
2020-03-28 23:07:34 -0700

    Update

--- R/cofactor.pca.cor.R
+++ R/cofactor.pca.cor.R
@@ -1,25 +1,23 @@
-#' Test for correlations between user-specified cofactors and principal components calculated from genotype data.
+#' Correlation between cofactors and principal components.
+#'
+#' \code{cofactor.pca.cor} Tests for correlations between user-specified cofactors and principal components calculated from genotype data.
 #'
 #' @param U A numeric matrix containing user-specified cofactors. Dimensions are n rows (individuals) by t columns (cofactors).
 #' @param G A numeric matrix containing genotype data. Dimensions are n rows (individuals) by m columns (genetic markers).
 #'
 #' @return A list of 1 or 3 objects.
 #'
+#' @description
 #' When U is unspecified, cofactor.pca.cor will return a list of 1 object.
-#'
 #' With U unspecified, function will carry out principal components analysis identically to the native R function prcomp(),
 #'  and cofactor.pca.cor will return principal components scores in $cov.
-#'
 #' $cov is a numeric matrix containing all principal components and individual scores.
 #'  Dimensions are n rows (individuals) by t columns (principal components).
 #'
 #' When U is specified, cofactor.pca.cor will return a list of 3 objects.
-#'
-#' $orig_pc is a numeric matrix containing all original principal components and individual scores
-#'
+#' $orig_pc is a numeric matrix containing all original principal components and individual scores.
 #' $cov is a numeric matrix containing user-specified cofactors and all principal components not correlated with the
 #'  user-specified cofactors. Dimensions are n rows (individuals) by t columns (cofactors).
-#'
 #' $removed is a character matrix indicating which principal components were removed.
 #'
 #' The $cov matrix is intended for use as the "C" argument in the GWASbyGLM function included in this package.
