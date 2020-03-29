df1eff2403c7691d30e631ef8087e6c5e00fd401
marcabeer <marcabeer@yahoo.com>
2020-03-29 00:21:11 -0700

    Added GLM-2020.R and GLM-2020.Rd, which will be used in making the manual.

--- R/GLM2020-package.R
+++ R/GLM2020-package.R
@@ -2,3 +2,5 @@
 #'
 #' @description This package offers several features for efficiently carrying out genome-wide association studies (GWAS) using a general linear model (GLM). Users may input phenotypic and genotypic data, as well as user-specified cofactors (e.g., location collected) and principal components calculated from genotype data.
 #' In addition to implementing GWAS using a GLM, this package offers functionality for automatically removing principal components that exhibit correlation with user-specified cofactors.
+
+"GLM2020-package"<-paste("GLM2020-package")
